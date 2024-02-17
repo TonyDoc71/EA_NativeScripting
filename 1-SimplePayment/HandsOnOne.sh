5c74971174da41b69a51f579ca27db7c13c4522a
TonyDoc71 <tony_livingston@live.com.au>
2024-02-17 13:52:49 +0000

    token

--- 1-SimplePayment/HandsOnOne.sh
+++ 1-SimplePayment/HandsOnOne.sh
@@ -3,15 +3,15 @@ cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.para
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
-  --tx-in "026432da32e2cc933316ad3f61a2917ad55736c5b8b91c090d9a95bf9f5b6dbd#1" \
-  --tx-out "addr_test1qpc6mrwu9cucrq4w6y69qchflvypq76a47ylvjvm2wph4szeq579yu2z8s4m4tn0a9g4gfce50p25afc24knsf6pj96sz35wnt"+"1100000" \
-  --change-address "addr_test1vzpjhv74y9sfykqtkkttdlf446z9ccadankd09vyvmkr3fg5a5acz" \
+  --tx-in "6a2d6721fde0880c0e9eaa267eb038f0abce7462b915dad0cc903299053922b6#0" \
+  --tx-out "addr_test1qpc6mrwu9cucrq4w6y69qchflvypq76a47ylvjvm2wph4szeq579yu2z8s4m4tn0a9g4gfce50p25afc24knsf6pj96sz35wnt"+"5000000 + 500 adfa35cc50c56a468394676d202c31194d3cd5197df266b6ed811def.416e74686f6e79436f696e" \
+  --change-address "addr_test1qz32zkseq8gz9ygphje3v2tzjggvarfveuzc6pd0age7yumayw8ggdd4v37lthf9pq4tn9pzq2v6njtn8s748wrkw9tqszuju7" \
   --protocol-params-file protocol.params \
   --out-file payment.unsigned
 
 cardano-cli transaction sign \
   --tx-body-file payment.unsigned \
-  --signing-key-file ../WalletMine/ent107.skey \
+  --signing-key-file ../WalletMine/5payment3.skey \
   --testnet-magic 2 \
   --out-file payment.signed
 
