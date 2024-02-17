dbcd263488ede531d5063ee4ed61d851c09f203b
TonyDoc71 <tony_livingston@live.com.au>
2024-02-17 05:31:16 +0000

    update

--- 4-MetaData/metadataTx.sh
+++ 4-MetaData/metadataTx.sh
@@ -3,15 +3,15 @@ cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.para
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
-  --tx-in "4b6bda5d37598571a89dc876885c5a973bb50e405a50a8545c6f65d395176739#2" \
-  --change-address $nami \
+  --tx-in "4e6e048d8a80650c7d0d525b691ac5980e21f647582bae2fb6002cff48f9c457#0" \
+  --change-address "addr_test1qra8rx05s9dv4690meheacnnjhs6uj49x24jmtp76e9c2ylede7uzn8enzys93d8735fa93ltmnpnp578vkhkf37a7eqwqcecv" \
   --metadata-json-file myData.json \
   --protocol-params-file protocol.params \
   --out-file metadataTx.unsigned
 
 cardano-cli transaction sign \
   --tx-body-file metadataTx.unsigned \
-  --signing-key-file ../../Wallets/Adr01.skey \
+  --signing-key-file ../WalletMine/batch107.skey \
   --testnet-magic 2 \
   --out-file metadataTx.signed
 
--- 4-MetaData/queryBlock.sh
+++ 4-MetaData/queryBlock.sh
@@ -1,1 +1,1 @@
-curl -H 'project_id: previewTQgzYm9vxX3Ot5sLA7m1YIpa5ckZNERq' https://cardano-preview.blockfrost.io/api/v0/metadata/txs/labels/1337 | jq
+curl -H 'project_id: previewTQgzYm9vxX3Ot5sLA7m1YIpa5ckZNERq' https://cardano-preview.blockfrost.io/api/v0/metadata/txs/labels/1708 | jq
